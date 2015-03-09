f89c330fcdf4030881c5420dd9117ce2a303e929
Alan James <alanjames1987@gmail.com>
2015-03-09 11:35:09 -0400

    Commit

--- app/src/main/java/com/alanjames1987/hotworddetector/HotwordService.java
+++ app/src/main/java/com/alanjames1987/hotworddetector/HotwordService.java
@@ -1,55 +1,55 @@
 package com.alanjames1987.hotworddetector;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.service.voice.AlwaysOnHotwordDetector;
 import android.service.voice.AlwaysOnHotwordDetector.Callback;
 import android.service.voice.VoiceInteractionService;
 import android.util.Log;
-import android.widget.Toast;
 
 import java.util.Locale;
 
 public class HotwordService extends VoiceInteractionService {
 
+    private final String LOG_TAG = "HotwordService";
+
+    Locale locale = new Locale("nl-el");
+
+    AlwaysOnHotwordDetector hotwordDetector;
+
     @Override
     public void onCreate() {
 
+        Log.i(LOG_TAG, "onCreate");
+
         super.onCreate();
-
-        ctx = getApplicationContext();
-
-        Log.i("AlanJamesVoice", "onCreate");
 
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Log.i(LOG_TAG, "onStartCommand");
+
         Bundle args = new Bundle();
+
         startSession(args);
         stopSelf(startId);
+
         return START_NOT_STICKY;
+
     }
 
     @Override
     public void onReady() {
 
+        Log.i(LOG_TAG, "onReady");
+
         super.onReady();
-
-        Log.i("AlanJamesVoice", "onReady");
-
-        Toast.makeText(ctx, "onReady", Toast.LENGTH_LONG).show();
 
         hotwordDetector = createAlwaysOnHotwordDetector("google", locale, callback);
 
     }
-
-    Context ctx;
-
-    Locale locale = new Locale("nl-el");
-
-    AlwaysOnHotwordDetector hotwordDetector;
 
     Callback callback = new Callback() {
 
@@ -66,14 +66,12 @@ public class HotwordService extends VoiceInteractionService {
         @Override
         public void onError() {
             // TODO Auto-generated method stub
-            Log.d("error", "error");
+            Log.d(LOG_TAG, "error");
         }
 
         @Override
         public void onDetected(AlwaysOnHotwordDetector.EventPayload eventPayload) {
-            // TODO Auto-generated method stub
-            // Display Toast message when Hotword is detected
-            Toast.makeText(ctx, "Google", Toast.LENGTH_LONG).show();
+            Log.d(LOG_TAG, "onDetected");
         }
 
         @Override
--- app/src/main/java/com/alanjames1987/hotworddetector/MainActivity.java
+++ app/src/main/java/com/alanjames1987/hotworddetector/MainActivity.java
@@ -4,17 +4,15 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+    private final String LOG_TAG = "MainActivity";
+
     Context ctx;
-    TextToSpeech speechOutput;
-    EditText input;
     Button button;
 
     @Override
@@ -26,23 +24,13 @@ public class MainActivity extends Activity {
 
         ctx = getApplicationContext();
 
-        speechOutput = new TextToSpeech(getApplicationContext(), null);
-        speechOutput.setSpeechRate((float) 1.2);
-
-        input = (EditText) findViewById(R.id.editText);
-
         button = (Button) findViewById(R.id.start);
 
         button.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
-//                Toast.makeText(ctx, "TOAST", Toast.LENGTH_LONG).show();
                 startService(new Intent(MainActivity.this, HotwordService.class));
-
-                CharSequence notification = (CharSequence) input.getText().toString();
-                speechOutput.speak(notification, TextToSpeech.QUEUE_ADD, null, "poop");
-
             }
 
         });
